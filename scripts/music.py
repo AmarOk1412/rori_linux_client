23f275b1feb2fa64efc27aad3994b6186caedddc
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2018-06-04 22:55:22 -0400

    music: use rhythmbox-client for more commands

--- scripts/music.py
+++ scripts/music.py
@@ -4,7 +4,17 @@ import subprocess
 if len(sys.argv) > 1:
     if sys.argv[1] == "start":
         print("start music")
-        subprocess.Popen("rhythmbox \"`find ~/Music/*.mp3 -type f | shuf -n 1`\"&", shell=True)
+        subprocess.Popen("rhythmbox-client --play", shell=True)
     if sys.argv[1] == "stop":
         print("stop music")
-        subprocess.Popen("pkill rhythmbox", shell=True)
+        subprocess.Popen("rhythmbox-client --stop", shell=True)
+    if sys.argv[1] == "pause":
+        print("pause music")
+        subprocess.Popen("rhythmbox-client --pause", shell=True)
+    if sys.argv[1] == "next":
+        print("next music")
+        subprocess.Popen("rhythmbox-client --next", shell=True)
+    if sys.argv[1] == "previous":
+        print("previous music")
+        subprocess.Popen("rhythmbox-client --previous", shell=True)
+        subprocess.Popen("rhythmbox-client --previous", shell=True)
